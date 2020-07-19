dd3582abbbac0a8c510939b278fcc37aeef20571
Brian Kresge <brian.kresge@gmail.com>
2020-07-19 08:37:32 -0400

    number of dev changes

--- password_decode.php
+++ password_decode.php
@@ -1,4 +1,7 @@
 <?php
-    echo (password_verify("3a7c33","$2y$10$57DrlQwRqOBtS4FRyoeRQOBIbERHSGuNnkYZ0WloOKYl8MMFpijbu"));
-    print_r(phpinfo());
+    //echo (password_verify("3a7c33","$2y$10$57DrlQwRqOBtS4FRyoeRQOBIbERHSGuNnkYZ0WloOKYl8MMFpijbu"));
+
+    echo (password_hash("NUgget38!@", PASSWORD_DEFAULT));
+
+    //print_r(phpinfo());
     ?>
