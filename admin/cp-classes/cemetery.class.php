de71738e4b4f062f5ed87d8cbc61845d7728b145
Brian Kresge <brian.kresge@gmail.com>
2021-01-30 06:47:18 -0500

    added cemetery

--- admin/cp-classes/cemetery.class.php
+++ admin/cp-classes/cemetery.class.php
@@ -4,7 +4,12 @@ class cemetery extends db
 {
     function get_cemeteries()
     {
-        $result = $this->get_array("SELECT * FROM `ppSD_cemetery`");
-        return $result;
+        $result = $this->run_query("SELECT * FROM `ppSD_cemetery`");
+        $cemeteries = array();
+        while ($row = $result->fetch())
+        {
+            $cemeteries[] = $row;
+        }
+        return $cemeteries;
     }
 }
