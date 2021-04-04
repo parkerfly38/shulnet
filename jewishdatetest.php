7369371c7267db85b8d9eed384bea276683e8291
Brian Kresge <brian.kresge@gmail.com>
2021-04-04 08:31:59 -0400

    updates

--- jewishdatetest.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: text/html; charset=utf-8');
-require "admin/sd-system/config.php";
-
-$jewishdate = new jewishdates();
-$dateToShow = $jewishdate->getHebrewFromGregorian(time());
-
-print_r($dateToShow);
-?>
