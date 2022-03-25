cddb22311bcb6455c414657d060349f1bf36cb54
Brian Kresge <brian.kresge@gmail.com>
2022-03-25 07:17:18 -0400

    updating family member

--- pp-functions/add_familymember.php
+++ pp-functions/add_familymember.php
@@ -10,3 +10,38 @@ if ($ses['error'] == '1')
     echo json_encode($result);
     exit;
 }
+$familymembers = new familymembers;
+
+$familymember = new familymember(
+    '', 
+    $ses['member_id'],
+    $_POST["firstname"],
+    $_POST["lastname"],
+    $_POST["addressline1"],
+    $_POST["addressline2"],
+    $_POST["city"],
+    $_POST["state"],
+    $_POST["zip"],
+    $_POST["country"],
+    $_POST["phone"],
+    $_POST["email"],
+    $_POST["DOB"],
+    $_POST["hebrewname"],
+    $_POST["bnaimitzvahdate"]);
+
+try {
+    $id = $familymembers->addFamilyMember($familmember);$result = array('fmid' => $id);
+    http_response_code(200);
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($result);
+    exit;
+}
+catch (Exception $exception)
+{
+    $result = array('error' => $exception);
+    http_response_code(500);
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($result);
+    exit;
+}
+?>
